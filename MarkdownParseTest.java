55dbb3f863d53f828f71daa1d4b68519ad627daf
Jackson-Wang-0 <zhw049@ucsd.edu>
2022-01-27 19:19:46 -0800

    more tests

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MarkdownParseTest {
     @Test
@@ -19,6 +20,25 @@ public class MarkdownParseTest {
 
         assertEquals("https://something.com",links.get(0));
         assertEquals("some-page.html",links.get(1));
+
+    }
+    @Test
+    public void testfile2() throws IOException {
+        Path fileName = Path.of("test-file2.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+
+        assertEquals("https://something.com",links.get(0));
+        assertEquals("some-page.html",links.get(1));
+
+    }
+    @Test
+    public void testfile3() throws IOException {
+        Path fileName = Path.of("test-file3.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+
+        assertEquals(List.of(),links);
 
     }
 }
